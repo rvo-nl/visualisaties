052854afda7e6da03b46edb0767eab6d600bcd46
tijslangeveld <tijslangeveld@gmail.com>
2024-12-24 10:33:26 +0100

    .

--- TVKN/A/v1/js/UIlisteners.js
+++ TVKN/A/v1/js/UIlisteners.js
@@ -11,25 +11,6 @@ function initializeUI () {
   if (document.getElementById('main-container').offsetWidth > maxWidth) {
     windowScaleValueInit = maxWidth / factor
   } else {windowScaleValueInit = document.getElementById('main-container').offsetWidth / 940 }
-
-// d3.select('#testSVG').attr('transform', `scale(${(document.getElementById('scaleableSVGContainer').offsetWidth-40) / (900)})`)
-// d3.select('#testSVG').attr('transform', 'scale(' + document.getElementById('scaleableSVGContainer').offsetWidth / 900).offsetWidth + ')'
-// var buttonsContainerHeight = document.getElementById('scenarioButtons').offsetHeight
-// d3.select('#buttonsContainer').style('height', buttonsContainerHeight + 35 + 50 + 'px')
-// var posTop = initMainContainerTop + buttonsContainerHeight
-// // console.log('Position Top:', posTop)
-// d3.select('#mainContainer').style('top', `${posTop}px`)
-// posTop = initYearButtonsTop + buttonsContainerHeight - 725
-// // console.log('Position Top:', posTop)
-// d3.select('#yearButtons').style('top', `${posTop}px`)
-// posTop = initJaarButtonsTitleTop + buttonsContainerHeight
-// d3.select('#jaarButtonsTitle').style('top', `${posTop}px`)
-// posTop = initUnitSelectorDivTop + buttonsContainerHeight
-// d3.select('#unitSelectorDiv').style('top', `${posTop}px`)
-// posTop = initRemarksDivTop + buttonsContainerHeight + document.getElementById('sankeyContainer').offsetHeight + document.getElementById('energieDragersContainer').offsetHeight
-// d3.select('#remarksContainer').style('top', `${posTop}px`)
-// posTop = initEnergieDragersDivTop + buttonsContainerHeight + document.getElementById('sankeyContainer').offsetHeight
-// d3.select('#energieDragersContainer').style('top', `${posTop}px`)
 }
 // Create a ResizeObserver instance
 const resizeObserver = new ResizeObserver((entries) => {
@@ -69,37 +50,6 @@ const resizeObserver = new ResizeObserver((entries) => {
 
     // console.log(document.getElementById('sankeySVGPARENT').getBoundingClientRect().height)
     d3.select('#scaleableSVGContainer').style('height', document.getElementById('sankeySVGPARENT').getBoundingClientRect().height + 'px')
-    // 
-    // // Dynamically adjust heights
-    // d3.select('#mainContainer').style('height', `${width * 1.2}px`)
-    // d3.select('#sankeyContainer').style('height', `${width * 0.62}px`)
-
-    // // Adjust buttons container height
-    // const buttonsContainerHeight = document.getElementById('scenarioButtons').offsetHeight
-    // d3.select('#buttonsContainer').style('height', `${buttonsContainerHeight + 35 +  50}px`)
-
-    // // Calculate and adjust top position dynamically
-    // var posTop = initMainContainerTop + buttonsContainerHeight
-    // d3.select('#mainContainer').style('top', `${posTop}px`)
-
-    // posTop = initYearButtonsTop + buttonsContainerHeight - 725
-    // d3.select('#yearButtons').style('top', `${posTop}px`)
-
-    // posTop = initJaarButtonsTitleTop + buttonsContainerHeight
-    // d3.select('#jaarButtonsTitle').style('top', `${posTop}px`)
-
-    // posTop = initUnitSelectorDivTop + buttonsContainerHeight
-    // d3.select('#unitSelectorDiv').style('top', `${posTop}px`)
-
-    // posTop = initRemarksDivTop + buttonsContainerHeight + document.getElementById('sankeyContainer').offsetHeight + document.getElementById('energieDragersContainer').offsetHeight
-    // d3.select('#remarksContainer').style('top', `${posTop}px`)
-    // // console.log(buttonsContainerHeight)
-    // posTop = initEnergieDragersDivTop + buttonsContainerHeight + document.getElementById('sankeyContainer').offsetHeight
-    // d3.select('#energieDragersContainer').style('top', `${posTop}px`)
-
-  // setWidth = document.getElementById('sankeyContainer').offsetWidth
-  // // console.log(setWidth)
-  // d3.select('#energieDragersContainer').style('width', `${setWidth*0.74}px`)
   }
 })
 
